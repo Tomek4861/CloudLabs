cd8532a73c8f1134d2f8b6689ab1a98af386e3fa
Tomek4861 <tomek4861g@gmail.com>
2025-04-01 00:09:24 +0200

    Added readme & license

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('user', views.user_delete, name='user_delete'),
     path('home', views.home, name='home'),
     path('contact', views.contact_page, name='contact'),
-    path('portfolio', views.portfolio_page, name='portfolio')
+    path('portfolio/', views.portfolio_page, name='portfolio')
     # path('portfolio/photo/<int:photo_id>/', views.portfolio_page, name='portfolio')
 ]
